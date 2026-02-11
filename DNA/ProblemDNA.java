94c0df37868ae127c4c1ce8ab58e346660c7c69b
philskocz <phil.skoczylas@kentshill.org>
2026-02-11 08:55:06 -0500

    Update ProblemDNA.java

--- DNA/ProblemDNA.java
+++ DNA/ProblemDNA.java
@@ -1,37 +1,27 @@
+public class DNACount {
+    public static void main(String[] args) {
 
-/**
- * Write a description of class ProblemDNA here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public class ProblemDNA
-{
-    public static void main() {
-        // replace this string with the one from Project Rosalind
-        final String data = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC";
-        final int[] exampleExpectedCounts = new int[]{20,12,17,21};
-        int[] output = nucleotideCounts(data);
-        // you can copy and paste from the terminal window into 
-        //   Project Rosalind to check your answer
-        System.out.println(formatData(output));
-    }
-    
-    // This should take the array of counts and return a 
-    //   space-delimited String with the respective counts
-    //   in A C G T order
-    private static String formatData(int[] counts) {
-        String output = "";
-        for( int count : counts ) {
-            output += count + " ";
+        String s = "GATACAAAGTCGGCCATCTTACGTTCAATGAATGTGGCATTATCGATTATCGTAGACACACATCCTGTGGCCGCCCCAGCGATTCGCCCCCATTAGAATGAACATAGGCACAATACATGAGTCTCTTCATGAATAGATCCCAGTGGTTATAAAGTGTGTCAAGCTCCCGTCTCGGCCTATGGCTAATCGCCAATGGAAACCGTGGAACCGTCCGTTGGACGATTTTACGGGAACAACATACACACGGGAATAGCCTACCCAACGAGGCTACTGTGCCACAGTGGTTGGGTTGTGCTACACGTACGATCCACGGAGTGAAAAATTTAGGAGCTCAAACAAAAATAAGACTGATTAGTATTACTTGTATGGGGTTAAGCACGTTGTTGAGCTCCCACGTCTGAAACGTGGTAGCGCCGCTCTTTGAGACTGAAACAGTTTGTAGGTCAGGCACAGGAGCATAGGGGGAAAGGAAAACACGTGCTTGACGGAGTAGGAGCCCCAATATTTCTGAAATGTGTTGCCAATCAGCGTGACCTTCAATGAGTACTAACCGGAGGGTATGCTCAATGCTCAGGCGTGTGGCATGGGCACGCATCTATTAGCAGTCCCGATAGAGACCTGTCCAGTCCTATACAGCTACGCTACTATGGCGGAGCATAAGGGCATGGGATCCGCGTATCCATACTTCCTCCAATTGCTGTACATTGGCGACTTAGTTTTTCGTAAGGGATGTAATAGATAAAAGATTACACCCAGTCCGTACACAAGCACTACGTACATTATCTTGGGGGATTATGTAAGAACACGAACTCTCAGATATGGTCCACATCTCGCGCGCATGCTTAAAGCATAGGTTTCTATTACGGGTAGACTCTAGGCAGTTTCTGCATTTAGATTGTGATCTAGTGGAGGAACGAGGAGCGCATGAGACTGTAGTTGTCGCCAGAATGGTCAGCCAAAGTGAACTGCC";
+
+        int countA = 0;
+        int countC = 0;
+        int countG = 0;
+        int countT = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char nucleotide = s.charAt(i);
+
+            if (nucleotide == 'A') {
+                countA++;
+            } else if (nucleotide == 'C') {
+                countC++;
+            } else if (nucleotide == 'G') {
+                countG++;
+            } else if (nucleotide == 'T') {
+                countT++;
+            }
         }
-        return output; 
-    }
-    
-    // This should take the given string and return an int
-    //   array of how many times each letter occurs in the 
-    //   string. Ideally in A C G T order. 
-    private static int[] nucleotideCounts(String dnaString) {
-        return new int[]{20,12,17,21};
+
+        System.out.println(countA + " " + countC + " " + countG + " " + countT);
     }
 }
